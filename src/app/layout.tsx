e6be3cf62ff5368574a2515aca9fbcc602bf3dba
dfuentmo <david.fuentes.monente@gmail.com>
2025-04-01 16:26:19 +0200

    fix layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
-// app/layout.js
+// app/layout.tsx
+import React from 'react';
 import { Inter } from 'next/font/google';
 import './globals.css';
 
@@ -9,7 +10,11 @@ export const metadata = {
   description: 'Celebrando lo mejor del bushcraft, la autosuficiencia y la comunidad de Wild Gnomos',
 };
 
-export default function RootLayout({ children }) {
+interface RootLayoutProps {
+  children: React.ReactNode;
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="es">
       <body className={inter.className}>
